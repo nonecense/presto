6fc830ba1c3987946133d61697b31a842b8f3b68
noncense <noncensexnoncense@gmail.com>
2017-04-27 23:04:53 +0900

    Update Orm.php

--- classes/utils/DB/Orm.php
+++ classes/utils/DB/Orm.php
@@ -1,10 +1,84 @@
 <?php
 namespace Presto\Util\DB;
 
+/**
+ * Oram::conditions($cond);
+ * [a][like]=['val', 'null|left|right']
+ * [a][between]=['min', 'max']
+ * [a][in]=[,,,,,]
+ *
+ * 検索条件
+ * 
+ * #1) a=1 and b=1
+ *  [a]=1
+ *  [b]=1
+ * #2) a=1 or b=1
+ *  [or][a]=1
+ *  [or][b]=1
+ * #3) a=1 and (b=1 or c=1)
+ *  [a]=1
+ *  [or][b]=1
+ *  [or][c]=1
+ * #4) (a=1 or b=1) and (c=1 or d=1)
+ *  [0][or][a]=1
+ *  [0][or][b]=1
+ *  [1][or][c]=1
+ *  [1][or][d]=1
+ * #5) (a=1 and b=1) or (c=1 or d=1)
+ *  [0][a]=1
+ *  [0][b]=1
+ *  [1][or][c]=1
+ *  [1][or][d]=1
+ *  
+ *  
+ * @return array 'bind'=>$bind,
+ * @return string 'conditions'=>sql where
+ */
 class Orm
 {
     public static function test()
     {
+        $cond = [];
+        $cond['a'] = 1;
+        $cond['b'] = 2;
+        var_dump(self::conditions($cond));
+
+        $cond = [];
+        $cond['or']['a'] = 1;
+        $cond['or']['b'] = 2;
+        var_dump(self::conditions($cond));
+
+        $cond = [];
+        $cond['or']['a'] = 1;
+        $cond['or']['b'] = 2;
+        $cond['c'] = 3;
+        var_dump(self::conditions($cond));
+
+        $cond = [];
+        $cond['or'][0]['a'] = 1;
+        $cond['or'][0]['b'] = 2;
+        $cond['or'][1]['c'] = 3;
+        $cond['or'][1]['d'] = 4;
+        var_dump(self::conditions($cond));
+
+        $cond = [];
+        $cond['or']['or']['a'] = 1;
+        $cond['or']['or']['b'] = 2;
+        $cond['or'][0]['a'] = 11;
+        $cond['or'][0]['b'] = 22;
+        $cond['or'][1]['c'] = 3;
+        $cond['or'][1]['d'] = 4;
+        var_dump(self::conditions($cond));
+
+        $cond = [];
+        $cond[0]['or']['or']['a'] = 1;
+        $cond[0]['or']['or']['b'] = 2;
+        $cond[0]['or'][0]['a'] = 11;
+        $cond[0]['or'][0]['b'] = 22;
+        $cond[1]['or'][1]['c'] = 3;
+        $cond[1]['or'][1]['d'] = 4;
+        var_dump(self::conditions($cond));
+
         $cond = [];
         $cond['user_id'] = 1;
         $cond['device_id'] = 'aaaaaaaa';
@@ -13,18 +87,9 @@ class Orm
         $cond[0]['or']['c'] = 3;
         $cond[0]['or']['d'] = 4;
         var_dump(self::conditions($cond));
-        
-        $cond = [];
-        $cond['or'][0]['a'] = 1;
-        $cond['or'][0]['b'] = 2;
-        $cond['or'][1]['c'] = 3;
-        $cond['or'][1]['d'] = 4;
-        
-        var_dump(self::conditions($cond));
     }
 
 
-    const ORM_KEY = ['or'];
     const SQL_OPERATORS = ['between', 'like', 'in', '>', '>=', '<', '<=', '<>', '='];
 
     public static $bind_serial_no = 0; // bindが重複しないように連番
@@ -32,11 +97,12 @@ class Orm
     // bind, where生成
     public static function conditions($params=[], $isOr=false, $clear_bind_serial_no=true)
     {
+        self::$bind_serial_no = $clear_bind_serial_no ? 0 : self::$bind_serial_no;
         $sql_where = "";
         $bind = [];
 
         foreach ($params as $key=>$param){
-            if(!is_array($param)){
+            if( ! is_array($param) ){
                 $bind_field_name = "{$key}_" . ++self::$bind_serial_no;
                 $bind[$bind_field_name] = $param;
                 if($isOr){
@@ -45,7 +111,25 @@ class Orm
                     $sql_where .= " AND {$key}=:'{$bind_field_name}': ";
                 }
             }elseif(!empty($param[0]) && in_array($param[0], self::SQL_OPERATORS)){
-                
+                switch ($param[0]){
+                    case 'between':
+                        $result = self::array_to_between($key, $param[1]);
+                        break;
+                    case 'like':
+                        $result = self::array_to_like($key, $param[1]);
+                        break;
+                    case 'in':
+                        $result = self::array_to_like($key, $param[1]);
+                        break;
+                    default:
+                        break;
+                }
+
+                if($isOr){
+                    $sql_where .= " {$key}=:'{$bind_field_name}': ";
+                }else{
+                    $sql_where .= " AND {$key}=:'{$bind_field_name}': ";
+                }
             }elseif(strtolower($key) == 'or'){
                 // or group
                 $sql_where_tmp = "";
@@ -71,10 +155,6 @@ class Orm
             }
         }
 
-        if($clear_bind_serial_no){
-            self::$bind_serial_no = 0;
-        }
-
         $sql_where = self::sanitize($sql_where);
         return ['conditions'=>$sql_where, 'bind'=>$bind];
     }
@@ -85,16 +165,17 @@ class Orm
      * @param unknown $field
      * @param unknown $cond [a][like]=>[val, position]
      */
-    public static function array_to_like($field, $val)
+    public static function array_to_like($key, $val)
     {
         $sql_where = "";
         $bind_field_name = $key .'_'. ++self::$bind_serial_no;
         if(empty($val[1])){
-            $sql_where = "AND {$key} {$val[0]} LIKE '%:{$bind_field_name}:%'";
+            
+            $sql_where = " {$key} {$val[0]} LIKE '%:{$bind_field_name}:%'";
         }elseif(strtolower($val[1]) == 'left'){
-            $sql_where = "AND {$key} {$val[0]} LIKE '%:{$bind_field_name}:'";
+            $sql_where = " {$key} {$val[0]} LIKE '%:{$bind_field_name}:'";
         }elseif(strtolower($val[1]) == 'right'){
-            $sql_where = "AND {$key} {$val[0]} LIKE ':{$bind_field_name}:%'";
+            $sql_where = " {$key} {$val[0]} LIKE ':{$bind_field_name}:%'";
         }else{
             throw new \Presto\Exception\ApplicationException("presto orm like conver error!");
         }
